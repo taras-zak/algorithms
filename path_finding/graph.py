863378c0f0466f8588207871bf5700fb43f5913f
taras-zak <taras.zakan@gmail.com>
2017-02-26 23:51:33 +0200

    update path_finding: draw labyrinth, find_path

--- path_finding/graph.py
+++ path_finding/graph.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 import collections
 
+#TODO: clean code, implement Dijkstra and A* algorithms
 labyrinth_example = [
 
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -58,6 +59,29 @@ class SimpleGraph:
                 result.append(Coord(row, col))
         return result
 
+    def draw(self, path_tracking=None, start=None, target=None, debug=True):
+        if debug:
+            print('   ', end='')
+            print(''.join("{:<2}".format(i) for i in  range(self.cols)))
+
+        for row in range(self.rows):
+            if debug:
+                print("{:2} ".format(row), end='')
+            for col in range(self.cols):
+                cell = ". "
+                if self.edges[row][col]:
+                    cell = '# '
+                if path_tracking and path_tracking.get(Coord(row,col), None) is not None:
+                    to = path_tracking[Coord(row,col)]
+                    if to.col == col + 1: cell = "\u2192 "
+                    if to.col == col - 1: cell = "\u2190 "
+                    if to.row == row + 1: cell = "\u2193 "
+                    if to.row == row - 1: cell = "\u2191 "
+                if start and start == Coord(row,col): cell = "S "
+                if target and target == Coord(row, col): cell = "T "
+                print(cell, end='')
+            print()
+
 Coord = collections.namedtuple('Node_coord', 'row,col')
 
 def breadth_first_search(graph, start, end):
@@ -68,7 +92,7 @@ def breadth_first_search(graph, start, end):
 
     while not frontier.empty():
         current = frontier.get()
-        print("Visititng {}".format(current))
+        #print("Visititng {}".format(current))
 
         if current == end:
             break
@@ -78,17 +102,34 @@ def breadth_first_search(graph, start, end):
             if next_ not in came_from:
                 frontier.put(next_)
                 came_from[next_] = current
+    return came_from
 
+def reconstruct_path(points, end):
+    path = []
+    current_cell = end
+    while points[current_cell]:
+        next = points[current_cell]
+        if current_cell.col + 1 == next.col: cell = "W"
+        if current_cell.col - 1 == next.col: cell = "E"
+        if current_cell.row + 1 == next.row: cell = "N"
+        if current_cell.row - 1 == next.row: cell = "S"
+        current_cell = next
+        path.append(cell)
+    return path
 
-graph = SimpleGraph(5, 5)
-graph.edges = labyrinth_example
-graph.cols = 12
-graph.rows = 12
-pprint(graph.edges)
-print(graph.neighbors(Coord(3,3)),end='\n\n')
+def main(maize_map):
+    graph = SimpleGraph(12, 12)
+    graph.edges = maize_map
 
-breadth_first_search(graph, Coord(1,1), Coord(10,10))
+    start = Coord(1,1)
+    target = Coord(10,10)
+    path = breadth_first_search(graph, start, target)
+    graph.draw(path, start, target)
 
+    print(list(reversed(reconstruct_path(path, Coord(10, 10)))))
+
+if __name__ == '__main__':
+    main(labyrinth_example)
 
 
 
