ce7da8d9cd03ab87b56b72d01b9a70bd6443a5ff
taras-zak <taras.zakan@gmail.com>
2021-06-23 03:17:19 +0700

    [DP] 1143.Longest_Common_Subsequence

---
+++ leetcode/1143.Longest_Common_Subsequence.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+	// var dp [1001][1001]int
+	dp := make([][]int, len(text1)+1)
+	for i := 0; i <= len(text1); i++ {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = 1 + dp[i-1][j-1]
+			} else {
+				dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+			}
+		}
+	}
+	return dp[len(text1)][len(text2)]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(longestCommonSubsequence("aggtab", "gxtxayb"))
+}
