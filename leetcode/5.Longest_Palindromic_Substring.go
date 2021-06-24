6cd378c7bc12f80ca8f5629f13a4c3faf18391cf
taras-zak <taras.zakan@gmail.com>
2021-06-24 23:59:36 +0700

    [STRING] 5.Longest_Palindromic_Substring

--- leetcode/5.Longest_Palindromic_Substring.go
+++ leetcode/5.Longest_Palindromic_Substring.go
@@ -25,6 +25,37 @@ func longestPalindromeDp(s string) string {
 	return s[palStart : palStart+palLen]
 }
 
+func longestPalindromeExpand(s string) string {
+	palLen := 1
+	palStart := 0
+	for i := 0; i < len(s); i++ {
+		one := expand(s, i, i)
+		two := expand(s, i, i+1)
+		maxLen := max(one, two)
+		if maxLen > palLen {
+			palStart = i - (maxLen-1)/2
+			palLen = maxLen
+		}
+	}
+	return s[palStart : palStart+palLen]
+}
+
+func expand(s string, left, right int) int {
+	for left >= 0 && right < len(s) && s[left] == s[right] {
+		left--
+		right++
+	}
+	return right - left - 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func main() {
 	fmt.Println(longestPalindromeDp("yabax"))
+	fmt.Println(longestPalindromeExpand("yabax"))
 }
